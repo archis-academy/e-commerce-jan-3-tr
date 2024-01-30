d6fcb9c61474abc0e1dcc72d9fb22aee455cb449
ali caliskan <caliskanali5534@gmail.com>
2024-01-30 16:12:31 +0300

    Javascript-Design

--- wishlist.js
+++ wishlist.js
@@ -18,7 +18,7 @@
   paketcard.innerHTML = paketler.map((paket) => 
    `
    <div class="container-card">
-   <img onlick="deleteurun(${paket.id})" class="icons" src="images/icon-delete.svg"/>
+   <img onclick="urunSil(1)" class="icons" src="images/icon-delete.svg"/>
      <img class="column-product" src="${paket.image}" alt="${paket.title}" />
      <p class="col-paragraph">Add To Cart</p>
      <img class="computer" src="images/Cart1.svg" alt="" />
@@ -59,4 +59,37 @@
   </div>
   `
  ).join("");
+
+ function urunSil(urunId) {
+    const silinecekIndex = paketler.findIndex((urun) => urun.id === urunId);
+
+    if (silinecekIndex !== -1) {
+      paketler.splice(silinecekIndex, 1);
+
+     
+      renderUrunler();
+      alert("Ürün başarıyla silindi!");
+    } else {
+      alert("Ürün bulunamadı!");
+    }
+  }
+  function renderUrunler() {
+    paketcard.innerHTML = paketler.map((paket) =>
+      `
+      <div class="container-card">
+        <img onclick="urunSil(${paket.id})" class="icons" src="images/icon-delete.svg"/>
+        <img class="column-product" src="${paket.image}" alt="${paket.title}" />
+        <p class="col-paragraph">Add To Cart</p>
+        <div>
+          <p class="explanation-product">${paket.title.substring(0, 20).concat(" ...")}</p>
+        </div>
+        <div class="prices">
+          <p>${paket.price}₺</p>
+        </div>
+      </div>
+      `
+    ).join("");
+  }
+  renderUrunler();
+
   
