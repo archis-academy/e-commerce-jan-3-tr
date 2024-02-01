805fb6c70c29c884c58a52bdaa16e56626558662
ali caliskan <caliskanali5534@gmail.com>
2024-02-01 16:30:55 +0300

    FİNİSH

--- wishlist.js
+++ wishlist.js
@@ -1,95 +1,39 @@
-  const adimlar =JSON.parse(localStorage.getItem("ürünler"));
-  
-  const kalanPaketler =[];
-  const paketler = [];
-  const justForYou=[];
-  const justForYouList =document.getElementById("justForYou");
-  justForYou.push(adimlar[1], adimlar[2], adimlar[3], adimlar[15]);
+const productcard = document.getElementById("productcard");
 
-  paketler.push(adimlar[8], adimlar[9], adimlar[10], adimlar[11]);
-  const paketcard = document.getElementById("paketcard");
-  function deleteurun(urunId) {
-     kalanPaketler = paketler.filter((urun) => {
-      return urun.id !== urunId;
-    });
-    console.log(paketler);
-  }
-  paketcard.innerHTML = paketler.map((paket) => 
-   `
-   <div class="container-card">
-   <img onclick="urunSil(1)" class="icons" src="images/icon-delete.svg"/>
-     <img class="column-product" src="${paket.image}" alt="${paket.title}" />
-     <p class="col-paragraph">Add To Cart</p>
-     <img class="computer" src="images/Cart1.svg" alt="" />
-     <div>
-       <p class="explanation-product">${paket.title
-           .substring(0, 20)
-           .concat(" ...")
-       }</p>
-     </div>
-     <div class="prices">
-       <p>${paket.price}₺</p>
-     </div>
-   </div>
-   `
-  ).join("");
+function renderUrunler() {
+  const wishlistProducts = JSON.parse(localStorage.getItem("wishlistProducts"));
 
-  justForYouList.innerHTML = justForYou.map((paket) => 
-  `
-  <div class="container-card">
-    <img class="column-product" src="${paket.image}" alt="${paket.title}" />
-    <p class="col-paragraph">Add To Cart</p>
-    <div>
-      <p class="explanation-product">${paket.title
-          .substring(0, 20)
-          .concat(" ...")
-      }</p>
-    </div>
-    <div class="prices">
-      <p>${paket.price}₺</p>
-    </div>
-    <div class="vectors">
-    <img class="vector" src="images/Vector.png" alt="" />
-    <img class="vector" src="images/Vector.png" alt="" />
-    <img class="vector" src="images/Vector.png" alt="" />
-    <img class="vector" src="images/Vector.png" alt="" />
-    <img class="vector" src="images/Vector.png" alt="" />
-  </div>
-  </div>
-  `
- ).join("");
-
- function urunSil(urunId) {
-    const silinecekIndex = paketler.findIndex((urun) => urun.id === urunId);
-
-    if (silinecekIndex !== -1) {
-      paketler.splice(silinecekIndex, 1);
-
-     
-      renderUrunler();
-      alert("Ürün başarıyla silindi!");
-    } else {
-      alert("Ürün bulunamadı!");
-    }
-  }
-  function renderUrunler() {
-    paketcard.innerHTML = paketler.map((paket) =>
-      `
+  productcard.innerHTML = wishlistProducts
+    .map(
+      (product) =>
+        `
       <div class="container-card">
-        <img onclick="urunSil(${paket.id})" class="icons" src="images/icon-delete.svg"/>
-        <img class="column-product" src="${paket.image}" alt="${paket.title}" />
+        <img onclick="deleteurun(${
+          product.id
+        })" class="icons" src="images/icon-delete.svg"/>
+        <img class="column-product" src="${product.image}" alt="${product.title}" />
         <p class="col-paragraph">Add To Cart</p>
         <div>
-          <p class="explanation-product">${paket.title.substring(0, 20).concat(" ...")}</p>
+          <p class="explanation-product">${product.title
+            .substring(0, 20)
+            .concat(" ...")}</p>
         </div>
         <div class="prices">
-          <p>${paket.price}₺</p>
+          <p>${product.price}₺</p>
         </div>
       </div>
       `
-    ).join("");
-  }
+    )
+    .join("");
+}
 
+renderUrunler();
+
+function deleteurun(urunId) {
+  const wishlistProducts = JSON.parse(localStorage.getItem("wishlistProducts"));
+  const kalanUrunler = wishlistProducts.filter((urun) => {
+    return urun.id !== urunId;
+  });
+  localStorage.setItem("wishlistProducts", JSON.stringify(kalanUrunler));
   renderUrunler();
-
-  
+}
