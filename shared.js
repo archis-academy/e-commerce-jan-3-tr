7be6956ee671dc341f083355b1787b426a7a185e
Meth <122786519+MethRAa@users.noreply.github.com>
2024-02-06 16:22:47 +0300

    [E--1]-wishlist restore

--- shared.js
+++ shared.js
@@ -154,8 +154,23 @@ getUrunlerByCategory().then((urunler) => {
         `<section class="productCard">
     <div class="box-img">
         <div class="column-icons">
-          <img src="images/heart small.png" alt="" />
-          <img src="images/Group.png" alt="" />
+          <div class="icon-box wishlist-click"> 
+          <svg class="icon-svg" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11 7C8.239 7 6 9.216 6 11.95C6 14.157 6.875 19.395 15.488 24.69C15.6423 24.7839 15.8194 24.8335 16 24.8335C16.1806 24.8335 16.3577 24.7839 16.512 24.69C25.125 19.395 26 14.157 26 11.95C26 9.216 23.761 7 21 7C18.239 7 16 10 16 10C16 10 13.761 7 11 7Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+          </div>
+          <div class="icon-box">
+          <svg fill="#000000" width="32px" height="32px" viewBox="-3.2 -3.2 38.40 38.40" version="1.1" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)">
+
+<g id="SVGRepo_bgCarrier" stroke-width="0"/>
+
+<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+
+<g id="SVGRepo_iconCarrier"> <path d="M31.739 8.875c-0.186-0.264-0.489-0.422-0.812-0.422h-21.223l-1.607-5.54c-0.63-2.182-2.127-2.417-2.741-2.417h-4.284c-0.549 0-0.993 0.445-0.993 0.993s0.445 0.993 0.993 0.993h4.283c0.136 0 0.549 0 0.831 0.974l5.527 20.311c0.12 0.428 0.511 0.724 0.956 0.724h13.499c0.419 0 0.793-0.262 0.934-0.657l4.758-14.053c0.11-0.304 0.064-0.643-0.122-0.907zM25.47 22.506h-12.046l-3.161-12.066h19.253zM23.5 26.504c-1.381 0-2.5 1.119-2.5 2.5s1.119 2.5 2.5 2.5 2.5-1.119 2.5-2.5c0-1.381-1.119-2.5-2.5-2.5zM14.5 26.504c-1.381 0-2.5 1.119-2.5 2.5s1.119 2.5 2.5 2.5 2.5-1.119 2.5-2.5c0-1.381-1.119-2.5-2.5-2.5z"/> </g>
+
+</svg>
+          </div>
+
         </div>
         <img class="product-images" src="${urun.image}" alt="${urun.title}" />
     </div>
@@ -190,4 +205,14 @@ logOut.addEventListener("click", () => {
 });
 if (isLogin == null) {
   window.location.href = "register.html";
+}
+const isWishClick = document.querySelectorAll(".icon-box");
+const wishBtnColor = document.querySelectorAll(".icon-svg");
+
+console.log(wishBtnColor);
+for (let i = 0; i < isWishClick.length; i++) {
+  isWishClick[i].addEventListener("click", () => {
+    console.log("test");
+    wishBtnColor[i].style.fill = "red";
+  });
 }
