07cfe0fa4277c5440f3fcc4564efb5593c124f93
ali caliskan <caliskanali5534@gmail.com>
2024-02-15 16:20:25 +0300

    [E-13-ABOUT-PAGE FİNİSH ]

--- about.js
+++ about.js
@@ -1,25 +1,22 @@
 const boxes = document.querySelectorAll(".box");
 
 boxes.forEach((box) => {
-    box.addEventListener("click" , ()=>{
-        boxes.forEach((box) => 
-        box.classList.remove("active"));
-        box.classList.add("active");
-    })
-});
-
-
-
-
-boxes.forEach((box) => {
-  box.addEventListener("click", () => {
-    boxes.forEach((otherBox) => {
-      otherBox.classList.remove('clicked');
+  box.addEventListener("click",() => {
+    boxes.forEach((box) =>
+      box.classList.remove("active"));
+      box.classList.add("active");
     });
+  })
 
-    box.classList.add('clicked');
-  });
-});
+boxes.forEach((box) => { 
+  box.addEventListener("click", () =>{
+    boxes.forEach((otherBox) => {
+      otherBox.classList.remove("clicked");
+    });
+    box.classList.add("clicked");
+  })
+})
+
 
 
 const buttons = document.querySelectorAll(".round-button");
@@ -31,37 +28,32 @@ buttons.forEach((button) => {
     button.classList.add('active');
   })
 })
-  
+
 const imageSets = [
-    ["images/image 46.png", "images/image 47.png", "images/image 51.png"],
-    ["images/resim1.jpg", "images/resim2.jpg", "images/resim3.webp"],
-    ["images/reism3.jpg", "images/image 46.png", "images/image 51.png"]
-  ];
-  
-  const roundButtons = document.querySelectorAll(".round-button");
-  const columnImages = document.querySelectorAll(".column-img");
-  
-  const imagesPerSet = 3;
-  
-  function updateColumnImages(startIndex) {
-    for (let i = 0; i < columnImages.length; i++) {
-      const imageIndex = startIndex + i;
-      if (imageIndex < imageSets[currentImageSetIndex].length) {
-        columnImages[i].src = imageSets[currentImageSetIndex][imageIndex];
-      } else {
-        columnImages[i].src = ""; 
-      }
+  ["images/image 46.png", "images/image 47.png", "images/image 51.png"],
+  ["images/resim1.jpg", "images/resim2.jpg", "images/resim3.webp"],
+  ["images/reism3.jpg", "images/image 46.png", "images/image 51.png"]
+];
+
+const roundButtons = document.querySelectorAll(".round-button");
+const columnImages = document.querySelectorAll(".column-img");
+let currentImageSetIndex = 0 ;
+updateColumnImages();
+function updateColumnImages(){
+  for(let i = 0; i < columnImages.length; i++){
+    columnImages[i].src=imageSets[currentImageSetIndex][i];
     }
-  }
-  
-  let currentImageSetIndex = 0;
-  
-  roundButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      currentImageSetIndex = index; 
-      updateColumnImages(0);
-    });
-  });
+}
+let imgIndex = 0;
+roundButtons.forEach((button,index) =>{
+  button.addEventListener("click" ,() => {
+    currentImageSetIndex=index;
+    updateColumnImages();
+  })
+})
+
+
+
 
   
  
