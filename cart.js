64628c80fdc1de4679fce8079b5edb5525b71df1
Meth <122786519+MethRAa@users.noreply.github.com>
2024-02-28 11:28:48 +0300

    [E--1] - cartProducts quantity subtotal actived.

--- cart.js
+++ cart.js
@@ -1,18 +1,26 @@
 /*Cart Page Start*/
 
-const productsLocal = JSON.parse(localStorage.getItem("cartProducts"));
 const table = document.getElementById("table");
 const setProducts = document.createElement("tbody");
 
-setProducts.innerHTML = productsLocal.map(
-  (product) =>
-    `<tr><td class="cart-table-body">
-    <span><img src="${product.image}" alt="${product.title}" /> ${product.title}</span>
+function renderProduct() {
+  const productsLocal = JSON.parse(localStorage.getItem("cartProducts"));
+  setProducts.innerHTML = productsLocal
+    .map(
+      (product) =>
+        `<tr><td class="cart-table-body">
+    <span>
+    <div class="cart-image-dlt-box">
+    <span onclick="deleteCartProduct(${product.id})" class="delete-cart">X</span>
+    <img class="cart-image" src="${product.image}" alt="${product.title}" />
+    </div>
+     ${product.title}
+     </span>
     </td>
     <td class="cart-table-body">$${product.price}</td>
     <td class="cart-table-body">
     <div class="cart-table-quantity-container">
-      <select class="cart-table-quantity" name="Quantity" id="cart-table-option">
+      <select onchange="subtotalCart(${product.id} , ${product.price})" class="cart-table-quantity" name="Quantity" id="cart-table-option-${product.id}">
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -26,8 +34,33 @@ setProducts.innerHTML = productsLocal.map(
       </select>
     </div>
     </td>
-    <td class="cart-table-body" id="cart-subtotal-${product.id}"></td></tr>`
-);
-
+    <td class="cart-table-body cart-subtotal" id="cart-subtotal-${product.id}">$${product.price}</td></tr>`
+    )
+    .join("");
+}
+renderProduct();
 table.appendChild(setProducts);
+function deleteCartProduct(productId) {
+  const newShopingProduct = cartProducts.filter(
+    (product) => product.id !== productId
+  );
+  localStorage.setItem("cartProducts", JSON.stringify(newShopingProduct));
+  cartProducts = newShopingProduct;
+  renderProduct();
+  noticeShopingCart();
+}
+function subtotalCart(productId, productPrice) {
+  const cartProducts = JSON.parse(localStorage.getItem("cartProducts"));
+  cartProducts.find((product) => {
+    return product.id === productId, product.price === productPrice;
+  });
+
+  const tableQuantity = document.querySelector(
+    `#cart-table-option-${productId}`
+  ).value;
+  const subtotalQuantity = document.querySelector(
+    `#cart-subtotal-${productId}`
+  );
+  subtotalQuantity.innerHTML = `$${tableQuantity * productPrice}`;
+}
 /*Cart Page End*/
